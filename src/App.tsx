a2df517ca6fcbfbc822cb7baf8624b9dfbf4ea35
zakki0925224 <zakki0925224@gmail.com>
2026-01-29 17:20:07 +0900

    [Add]: Added simple template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+export function App() {
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    );
+}
+
+export default App;
